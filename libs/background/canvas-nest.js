8a3e2e73ccaa0460e25ee0171e08919f2cf27219
sxk <284254231@qq.com>
2024-12-03 15:35:26 +0800

    Site updated: 2024-12-03 15:35:25

--- libs/background/canvas-nest.js
+++ libs/background/canvas-nest.js
@@ -1,1 +1,92 @@
-(()=>{for(var n=navigator.userAgent,e=["iPad","iPhone","Android","SymbianOS","Windows Phone","iPod","webOS","BlackBerry","IEMobile"],t=0;t<e.length;t++)if(0<n.indexOf(e[t]))return;function i(n,e,t){return n.getAttribute(e)||t}function o(n){return document.getElementsByTagName(n)}function a(){l=m.width=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,u=m.height=window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight}function c(){w.clearRect(0,0,l,u);var e,t,i,o,a,d=[h].concat(f);f.forEach(function(n){for(n.x+=n.xa,n.y+=n.ya,n.xa*=l<n.x||n.x<0?-1:1,n.ya*=u<n.y||n.y<0?-1:1,w.fillRect(n.x-.5,n.y-.5,1,1),t=0;t<d.length;t++)n!==(e=d[t])&&null!==e.x&&null!==e.y&&(i=n.x-e.x,o=n.y-e.y,(a=i*i+o*o)<e.max)&&(e===h&&a>=e.max/2&&(n.x-=.03*i,n.y-=.03*o),i=(e.max-a)/e.max,w.beginPath(),w.lineWidth=i/2,w.strokeStyle="rgba("+r.c+","+(.2+i)+")",w.moveTo(n.x,n.y),w.lineTo(e.x,e.y),w.stroke());d.splice(d.indexOf(n),1)}),y(c)}var l,u,d,m=document.createElement("canvas"),r=(d=o("script"),{l:x=d.length,z:i(d=d[x-1],"zIndex",-1),o:i(d,"opacity",.5),c:i(d,"color","0,0,0"),n:i(d,"count",99)}),x="c_n"+r.l,w=m.getContext("2d"),y=window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(n){window.setTimeout(n,1e3/45)},s=Math.random,h={x:null,y:null,max:2e4};m.id=x,m.style.cssText="position:fixed;top:0;left:0;z-index:"+r.z+";opacity:"+r.o,o("body")[0].appendChild(m),a(),window.onresize=a,window.onmousemove=function(n){n=n||window.event,h.x=n.clientX,h.y=n.clientY},window.onmouseout=function(){h.x=null,h.y=null};for(var f=[],g=0;g<r.n;g++){var b=s()*l,v=s()*u,p=2*s()-1,A=2*s()-1;f.push({x:b,y:v,xa:p,ya:A,max:6e3})}setTimeout(function(){c()},100)})();
+! function () {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["iPad", "iPhone", "Android",
+        "SymbianOS", "Windows Phone",
+        "iPod", "webOS", "BlackBerry", "IEMobile"
+    ];
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            return;
+        }
+    }
+
+    function o(w, v, i) {
+        return w.getAttribute(v) || i
+    }
+
+    function j(i) {
+        return document.getElementsByTagName(i)
+    }
+
+    function l() {
+        var i = j("script"),
+            w = i.length,
+            v = i[w - 1];
+        return {
+            l: w,
+            z: o(v, "zIndex", -1),
+            o: o(v, "opacity", 0.5),
+            c: o(v, "color", "0,0,0"),
+            n: o(v, "count", 99)
+        }
+    }
+
+    function k() {
+        r = u.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, n = u.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    }
+
+    function b() {
+        e.clearRect(0, 0, r, n);
+        var w = [f].concat(t);
+        var x, v, A, B, z, y;
+        t.forEach(function (i) {
+            i.x += i.xa, i.y += i.ya, i.xa *= i.x > r || i.x < 0 ? -1 : 1, i.ya *= i.y > n || i.y < 0 ? -1 : 1, e.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
+            for (v = 0; v < w.length; v++) {
+                x = w[v];
+                if (i !== x && null !== x.x && null !== x.y) {
+                    B = i.x - x.x, z = i.y - x.y, y = B * B + z * z;
+                    y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, e.beginPath(), e.lineWidth = A / 2, e.strokeStyle = "rgba(" + s.c + "," + (A + 0.2) + ")", e.moveTo(i.x, i.y), e.lineTo(x.x, x.y), e.stroke())
+                }
+            }
+            w.splice(w.indexOf(i), 1)
+        }), m(b)
+    }
+    var u = document.createElement("canvas"),
+        s = l(),
+        c = "c_n" + s.l,
+        e = u.getContext("2d"),
+        r, n, m = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (i) {
+            window.setTimeout(i, 1000 / 45)
+        },
+        a = Math.random,
+        f = {
+            x: null,
+            y: null,
+            max: 20000
+        };
+    u.id = c;
+    u.style.cssText = "position:fixed;top:0;left:0;z-index:" + s.z + ";opacity:" + s.o;
+    j("body")[0].appendChild(u);
+    k(), window.onresize = k;
+    window.onmousemove = function (i) {
+        i = i || window.event, f.x = i.clientX, f.y = i.clientY
+    }, window.onmouseout = function () {
+        f.x = null, f.y = null
+    };
+    for (var t = [], p = 0; s.n > p; p++) {
+        var h = a() * r,
+            g = a() * n,
+            q = 2 * a() - 1,
+            d = 2 * a() - 1;
+        t.push({
+            x: h,
+            y: g,
+            xa: q,
+            ya: d,
+            max: 6000
+        })
+    }
+    setTimeout(function () {
+        b()
+    }, 100)
+}();
